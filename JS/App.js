696baab8fa30118db62ff2a4c1025cd828ecb05b
Kamil <kamil.m.olsza@gmail.com>
2019-03-21 14:19:22 +0100

    List with delete working and edit working

--- JS/App.js
+++ JS/App.js
@@ -3,16 +3,6 @@
 const ol = document.querySelector('ol');
 const body = document.querySelector('body')
 
-let handleClear = function(){
-  localStorage.clear();
-}
-
-let clearBtn = document.createElement('input');
-  clearBtn.setAttribute('type', 'submit');
-  clearBtn.value = "Clear";
-  clearBtn.addEventListener("click", handleClear);
-  body.appendChild(clearBtn);
-
 document.getElementById("newNote").addEventListener("click", addNote);
 
 function addNote() {
@@ -21,9 +11,10 @@ function addNote() {
   let divInput = document.createElement('div');
   divInput.setAttribute('class', 'form-group');
   let input = document.createElement('input');
-  input.setAttribute('type', 'text', 'class', 'form-control', 'id', 'title'); // why adding multiple attributes don't work?
+  input.setAttribute('type', 'text', 'class', 'form-control', 'id', 'title'); // adding multiple attributes don't work?
   input.id = 'title';
   input.value = "Title";
+  // input.addEventListener("input",handleAutoSave);
   divInput.appendChild(input);
 
   let divTextarea = document.createElement('div');
@@ -32,20 +23,23 @@ function addNote() {
   textArea.setAttribute('class', 'form-control');
   textArea.id = 'content';
   textArea.value = "notes...";
+  // textArea.addEventListener("input", handleAutoSave);
   divTextarea.appendChild(textArea);
 
   // Save button logic - according to requirements it should be auto-save
   let saveBtn = document.createElement('input');
   saveBtn.setAttribute('type', 'submit');
   saveBtn.value = "Save";
-  saveBtn.addEventListener("click", handleSaveClick);
+  saveBtn.addEventListener("click", handleAutoSave);
 
   parent.appendChild(divInput);
   parent.appendChild(divTextarea);
   parent.appendChild(saveBtn);
+
+  console.log('addNote function fired');
 }
 
-let handleSaveClick = function () {
+let handleAutoSave = function () {
   // create localStorage file is it doesn't exist
   if (JSON.parse(localStorage.getItem("notes")) === null) {
     let notes = new Array();
@@ -75,19 +69,19 @@ let handleSaveClick = function () {
 
   // save new invoice in localStorage
   localStorage.setItem("notes", JSON.stringify(notes));
-
+  console.log('note: ' + note.title + 'saved to localstorage');
 }
 
 let liMaker = function (title, date, arrayIndex, notesToWeb) {
   let editBtn = document.createElement('input');
   editBtn.setAttribute('type', 'submit');
   editBtn.value = "Edit";
-  // editBtn.addEventListener("click", handleEditClick(arrayIndex));  
+  editBtn.addEventListener("click", function (){handleEditClick(arrayIndex)});
 
   let deleteBtn = document.createElement('input');
   deleteBtn.setAttribute('type', 'submit');
   deleteBtn.value = "X";
-  // deleteBtn.addEventListener("click", handleDeleteClick(arrayIndex, notesToWeb));
+  deleteBtn.addEventListener("click", function (){handleDeleteClick(arrayIndex, notesToWeb)});
 
   const li = document.createElement('li');
   li.textContent = title + " \u000B " + date + " | "; // \u000B should be tabulator but it don't work
@@ -128,14 +122,21 @@ let handleEditClick = function (arrayIndex) {
   let saveBtn = document.createElement('input');
   saveBtn.setAttribute('type', 'submit');
   saveBtn.value = "Save";
-  saveBtn.addEventListener("click", handleSaveClick);
+  saveBtn.addEventListener("click", handleAutoSave);
 
   parent.appendChild(divInput);
   parent.appendChild(divTextarea);
   parent.appendChild(saveBtn);
 }
 
-window.onload = event => {
+let handleClear = function () {
+  console.log('handleClear fired');
+  localStorage.clear();
+}
+
+let displayNotes = function () {
+  console.log("display")
+
   var notesToWeb = JSON.parse(localStorage.getItem("notes"));
   // debugger
   var arrayIndex = 0;
@@ -147,19 +148,25 @@ window.onload = event => {
       arrayIndex = i;
       liMaker(noteTitle, noteDate, arrayIndex, notesToWeb);
     }
-    // notesToWeb.forEach(note => {
-    //   let noteTitle = note.title;
-    //   let noteDate = note.noteDate;
-
-    //   liMaker(noteTitle, noteDate, arrayIndex);
-    //   arrayIndex +=1;
-    // });
-    
   }
+  let clearBtn = document.createElement('input');
+  clearBtn.setAttribute('type', 'submit');
+  clearBtn.value = "Clear";
+  clearBtn.addEventListener("click", handleClear);
+  document.getElementById('list').appendChild(clearBtn);
 }
 
 let handleDeleteClick = function (arrayIndex, notesToWeb) {
-  delete notesToWeb[arrayIndex];
-  // save new invoice in localStorage
-  localStorage.setItem("notes", JSON.stringify(notesToWeb));
-}
+  document.getElementById('list').innerHTML = '';
+    console.log("clear");
+   notesToWeb.splice(arrayIndex,1);
+   // delete notesToWeb[arrayIndex];
+   // save new invoice in localStorage
+   localStorage.setItem("notes", JSON.stringify(notesToWeb));
+   console.log("next display")
+   displayNotes();
+
+  }
+
+
+window.onload = displayNotes();
